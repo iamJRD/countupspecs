58f48b9b158ebaf62ad04ea35e655acbb5046293
jared beckler and nic netzel <netzeln@gmail.com>
2016-01-21 09:44:21 -0800

    add first spec test

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+var countUp = function(maxNumber) {
+  var values = [];
+
+  for(var i = 1; i <= maxNumber; i++){
+      values.push(i);
+  }
+  return values.join(",");
+}
+
+
+
+
+
+
 $(document).ready(function() {
 
 
---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe("countUp",function(){
+  it("counts up to a specified number", function(){
+    expect(countUp("3")).to.equal("1,2,3");
+  });
+
+});
